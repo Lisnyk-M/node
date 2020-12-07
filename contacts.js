59d861fe80f2ae80de0aefd39601b0fe0c4e99ed
Lisnyk-M <swapbuffers@gmail.com>
2020-12-07 21:05:50 +0200

    added readme

--- contacts.js
+++ contacts.js
@@ -46,6 +46,7 @@ function removeContact(contactId) {
     fsReadFile(contactsPath).then(res => {
         const contact = res.filter(el => el.id !== contactId);
         fsWriteFile(contact);
+        console.log(`Contact id=${contactId} was removed`);
     });
 }
 
@@ -53,12 +54,24 @@ function addContact(name, email, phone) {
     let maxId = 0;
 
     fsReadFile(contactsPath).then(res => {
-        res.forEach(element => {
-            if (element.id > maxId) {maxId = element.id} 
-        });
-        const id = maxId + 1;
-        const data = [...res, {id, name, email, phone }];
-        fsWriteFile(data);        
+        const findName = res.find(el => el.name === name);
+
+        if (!findName) {
+            res.forEach(element => {
+                if (element.id > maxId) { maxId = element.id }
+            });
+
+            const id = maxId + 1;
+            const contact = {  id, name, email, phone }
+
+            const data = [...res, contact];
+            fsWriteFile(data);
+            console.log('contact is added:');
+            console.table(contact);
+
+        } else {
+            console.log('contact already in database');
+        }
     });
 }
 
